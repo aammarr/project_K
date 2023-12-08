4a9300829e27adb28ad475cc57051516106e5e05
Ammar Ahmed <aammarr.ahmed@gmail.com>
2023-12-08 16:08:29 +0500

    update db seeds

---
+++ sites/backend/config/db_seeds.sql
@@ -0,0 +1,11 @@
+/*----------
+ | DB Seeds |
+ ----------*/
+
+INSERT INTO
+    `project_k`.`roles` (`role_id`, `role_name`)
+VALUES ('1', 'superadmin');
+
+INSERT INTO
+    `project_k`.`roles` (`role_id`, `role_name`)
+VALUES ('2', 'user');
