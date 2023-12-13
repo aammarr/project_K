3c620cdc925b4afe75519a335c12d3e172bd870c
moazzam2019 <moazzam.iqbal2019@gmail.com>
2023-12-13 13:24:53 +0500

    login page error fixed

--- sites/admin/src/views/pages/login/Login.js
+++ sites/admin/src/views/pages/login/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import {
   CButton,
@@ -12,6 +12,7 @@ import {
   CInputGroup,
   CInputGroupText,
   CRow,
+  CSpinner,
 } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
 import { cilLockLocked, cilUser } from '@coreui/icons'
@@ -24,9 +25,26 @@ const Login = () => {
   const navigate = useNavigate()
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    // Check if x-auth-token exists in local storage
+    const authToken = localStorage.getItem('x-auth-token')
+    if (authToken) {
+      // Redirect to dashboard if token exists
+      navigate('/dashboard')
+    }
+  }, [])
 
   const handleLogin = async () => {
     try {
+      if (!email || !password) {
+        toast.error('Please enter both email and password', {
+          position: toast.POSITION.TOP_RIGHT,
+        })
+        return
+      }
+      setLoading(true)
       // Make sure to replace 'YOUR_API_ENDPOINT' with the actual endpoint
       const response = await axios.post(`${process.env.REACT_APP_BASE_URL}user/login`, {
         email,
@@ -43,7 +61,6 @@ const Login = () => {
       localStorage.setItem('x-auth-token', userData.token)
 
       // Display toast for successful login
-
       if (userData?.token) {
         toast.success('Login successful', { position: toast.POSITION.TOP_RIGHT })
 
@@ -55,8 +72,16 @@ const Login = () => {
       toast.error(error?.response?.data?.message, {
         position: toast.POSITION.TOP_RIGHT,
       })
+    } finally {
+      setLoading(false)
     }
   }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault() // Prevent the default form submission
+    await handleLogin() // Call your handleLogin function
+  }
+
   return (
     <div className="bg-light min-vh-100 d-flex flex-row align-items-center">
       <CContainer>
@@ -65,7 +90,7 @@ const Login = () => {
             <CCardGroup>
               <CCard className="p-4">
                 <CCardBody>
-                  <CForm>
+                  <CForm onSubmit={handleSubmit}>
                     <h1>Login</h1>
                     <p className="text-medium-emphasis">Sign In to your account</p>
                     <CInputGroup className="mb-3">
@@ -91,8 +116,21 @@ const Login = () => {
                     </CInputGroup>
                     <CRow>
                       <CCol xs={6}>
-                        <CButton color="primary" className="px-4" onClick={handleLogin}>
-                          Login
+                        <CButton type="submit" color="primary" className="px-4" disabled={loading}>
+                          {loading ? (
+                            <>
+                              <CSpinner
+                                as="span"
+                                size="sm"
+                                animation="border"
+                                role="status"
+                                aria-hidden="true"
+                              />
+                              <span style={{ marginLeft: '5px' }}> Login </span>
+                            </>
+                          ) : (
+                            'Login'
+                          )}
                         </CButton>
                       </CCol>
                     </CRow>
