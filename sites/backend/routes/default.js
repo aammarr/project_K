f4cfd6c9326a49bc25b3779867cdc54f0887efe5
Ammar Ahmed <aammarr.ahmed@gmail.com>
2024-01-09 00:58:26 +0500

    V.2024.01.09

--- sites/backend/routes/default.js
+++ sites/backend/routes/default.js
@@ -4,7 +4,7 @@ const router = Router.Router();
 router.get("/", (req, res) => {
     res.send({
         message: 'Welcome to Project K',
-        version: '2023.12.20'
+        version: '2024.01.09'
     });
 });
 
