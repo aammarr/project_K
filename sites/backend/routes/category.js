0689f5a30003004101789bad6836b516f1d135c0
moazzam2019 <moazzam.iqbal2019@gmail.com>
2024-01-10 16:56:27 +0500

    auth removed from cat and temp

--- sites/backend/routes/category.js
+++ sites/backend/routes/category.js
@@ -1,11 +1,17 @@
 import auth from "../middleware/auth.js";
-import categoryController from '../controller/categoryController.js';
+import categoryController from "../controller/categoryController.js";
 import Router from "express";
 
 const router = Router.Router();
-const { createCategory, getCategories, getCategoryById, updateCategoryById, deleteCategoryById } = categoryController;
+const {
+  createCategory,
+  getCategories,
+  getCategoryById,
+  updateCategoryById,
+  deleteCategoryById,
+} = categoryController;
 
-router.get("/", auth, getCategories);
+router.get("/", getCategories);
 router.post("/", auth, createCategory);
 router.get("/:id", auth, getCategoryById);
 router.put("/:id", auth, updateCategoryById);
--- sites/backend/routes/template.js
+++ sites/backend/routes/template.js
@@ -1,28 +1,39 @@
 import auth from "../middleware/auth.js";
-import templateController from '../controller/templateController.js';
+import templateController from "../controller/templateController.js";
 import Router from "express";
-import AWS from 'aws-sdk';
+import AWS from "aws-sdk";
 
-import multer from 'multer';
-import multerS3, { AUTO_CONTENT_TYPE } from 'multer-s3';
+import multer from "multer";
+import multerS3, { AUTO_CONTENT_TYPE } from "multer-s3";
 
 // import uploadHelper from "../helper/upload.js";
 // const { upload } = uploadHelper;
 const router = Router.Router();
-const { createTemplate, getAllTemplates, getTemplateById, updateTemplateById, deleteTemplateById, getAllTemplatesByCategoryId,
-    getUploadId, getSignedUrlMultipPart, fileSaveIntoDb, completeMultipartUpload, getPutSignedUrl,getDownloadUrl } = templateController;
+const {
+  createTemplate,
+  getAllTemplates,
+  getTemplateById,
+  updateTemplateById,
+  deleteTemplateById,
+  getAllTemplatesByCategoryId,
+  getUploadId,
+  getSignedUrlMultipPart,
+  fileSaveIntoDb,
+  completeMultipartUpload,
+  getPutSignedUrl,
+  getDownloadUrl,
+} = templateController;
 
+router.get("/getPutSignedUrl", getPutSignedUrl);
 
-router.get('/getPutSignedUrl', getPutSignedUrl);
+router.post("/initiateUpload", getUploadId);
+router.post("/getSignedUrlMultipart", getSignedUrlMultipPart);
+router.post("/complete-multipart-upload", completeMultipartUpload);
+router.post("/file-save-into-db", fileSaveIntoDb);
 
-router.post('/initiateUpload', getUploadId);
-router.post('/getSignedUrlMultipart', getSignedUrlMultipPart);
-router.post('/complete-multipart-upload', completeMultipartUpload);
-router.post('/file-save-into-db', fileSaveIntoDb);
+router.get("/getSignedUrlDownload", getDownloadUrl);
 
-router.get('/getSignedUrlDownload', getDownloadUrl);
-
-router.get("/", auth, getAllTemplates);
+router.get("/", getAllTemplates);
 router.get("/category/:category_id", auth, getAllTemplatesByCategoryId);
 router.post("/", auth, createTemplate);
 router.get("/:id", auth, getTemplateById);
@@ -32,42 +43,40 @@ router.delete("/:id", auth, deleteTemplateById);
 // upload
 
 // Configure AWS SDK with your credentials
-    AWS.config.update({
-        accessKeyId: process.env.AWS_ACCESS_KEY,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-        bucketURL: process.env.AWS_BUCKET
-    });
-    
-    // Create an S3 instance
-    const s3 = new AWS.S3();
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  bucketURL: process.env.AWS_BUCKET,
+});
 
-    // Set up multer for handling file uploads
-    const storage = multer.memoryStorage();
-    const upload = multer({ storage: storage });
+// Create an S3 instance
+const s3 = new AWS.S3();
 
+// Set up multer for handling file uploads
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
-router.post('/media', upload.single('file'),  async (req, res) => {
-    const bucketName = 'project-k-templates';
-    const fileName = Date.now().toString()+"_"+req.file.originalname;
-    const fileContent = req.file.buffer;
+router.post("/media", upload.single("file"), async (req, res) => {
+  const bucketName = "project-k-templates";
+  const fileName = Date.now().toString() + "_" + req.file.originalname;
+  const fileContent = req.file.buffer;
 
-    // Set the parameters for S3 upload with public-read ACL
-    const params = {
-        Bucket: bucketName,
-        Key: fileName,
-        Body: fileContent,
-    };
-    // Upload the file to S3
-    s3.upload(params, (err, data) => {
-        if (err) {
-            console.error('Error uploading file to S3:', err);
-            res.status(500).send('Error uploading file to S3');
-        } else {
-            const publicUrl = data.Location;
-            return res.send({ 'public_url':publicUrl }).status(200);
-        }
-    });
+  // Set the parameters for S3 upload with public-read ACL
+  const params = {
+    Bucket: bucketName,
+    Key: fileName,
+    Body: fileContent,
+  };
+  // Upload the file to S3
+  s3.upload(params, (err, data) => {
+    if (err) {
+      console.error("Error uploading file to S3:", err);
+      res.status(500).send("Error uploading file to S3");
+    } else {
+      const publicUrl = data.Location;
+      return res.send({ public_url: publicUrl }).status(200);
+    }
   });
-
+});
 
 export default router;
